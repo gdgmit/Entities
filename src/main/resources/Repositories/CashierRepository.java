1e36eabe22c334bc15dc181895f05d8e343cea70
Maanasa Prathap <maanasaprathapchander@gmail.com>
2024-12-29 22:01:22 +0530

    Create CashierRepository.java

---
+++ src/main/resources/Repositories/CashierRepository.java
@@ -0,0 +1,7 @@
+package com.example.entities.Repositories;
+
+import com.example.entities.Entities.Cashier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CashierRepository extends JpaRepository<Cashier, Long> {
+}
