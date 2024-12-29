a1e5011dd8258a847e8080862ba6092da0ce9c5b
Maanasa Prathap <maanasaprathapchander@gmail.com>
2024-12-29 14:26:01 +0530

    Create RolePermission.java

---
+++ src/test/java/com/example/entities/Entities/RolePermission.java
@@ -0,0 +1,29 @@
+package com.example.entities.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+@Table(name = "Role_Permission")
+public class RolePermission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "roleId", nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "permissionId", nullable = false)
+    private Permission permission;
+}
