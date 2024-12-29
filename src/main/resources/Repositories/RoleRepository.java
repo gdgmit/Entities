d90cc685cd61e4af82d23d96d730913603383132
Maanasa Prathap <maanasaprathapchander@gmail.com>
2024-12-29 22:02:16 +0530

    Create RoleRepository.java

---
+++ src/main/resources/Repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.entities.Repositories;
+
+import com.example.entities.Entities.RolePermission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {
+}
