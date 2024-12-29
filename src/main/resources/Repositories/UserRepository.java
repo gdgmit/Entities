9bbfc488515691edc980d77040a7881aee5ac4d2
Maanasa Prathap <maanasaprathapchander@gmail.com>
2024-12-29 22:02:31 +0530

    Create UserRepository.java

---
+++ src/main/resources/Repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.entities.Repositories;
+
+import com.example.entities.Entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
