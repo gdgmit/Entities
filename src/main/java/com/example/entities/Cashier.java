cd99c6b33c55454900f7bb0749bbb7f10b2e15e8
Maanasa Prathap <maanasaprathapchander@gmail.com>
2024-12-29 21:55:22 +0530

    Create Cashier.java

---
+++ src/main/java/com/example/entities/Cashier.java
@@ -0,0 +1,25 @@
+package com.example.entities.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import java.util.Set;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+@Table(name = "Cashiers")
+public class Cashier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cashierId;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String cashierName;
+
+    // for any more relavant fields, make changges here
+}
